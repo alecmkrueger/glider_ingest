9ae99031a7fd2e4ac19cf8721713f5ec16470cb1
Alec Krueger <Alecmkrueger@tamu.edu>
2025-01-24 16:58:50 -0600

    add two examples under adding custom variables

--- docs/examples/add_custom_variables.py
+++ docs/examples/add_custom_variables.py
@@ -15,7 +15,17 @@ def main():
 
     # Add custom variables to the mission_data container
     m_water_vx = Variable(data_source_name='m_water_vx')  # Initialize the variable
+    m_water_vx.short_name = 'vx_currents'  # Variable name in the netcdf file
+    m_water_vx.long_name = 'x-component of water velocity'
+    m_water_vx.units = 'm/s'
+    m_water_vx.comment = 'This is a comment'
+    
     m_water_vy = Variable(data_source_name='m_water_vy')  # Initialize the variable
+    m_water_vy.short_name = 'vy_currents'  # Variable name in the netcdf file
+    m_water_vy.long_name = 'y-component of water velocity'
+    m_water_vy.units = 'm/s'
+    m_water_vy.comment = 'This is a comment'
+    
     mission_data.add_variables(variables=[m_water_vx,m_water_vy])  # Add the variables as a list of Variable objects
     # Pass the mission_data container to the MissionProcessor class
     # call save_mission_dataset to generate and save the mission dataset
--- docs/examples/add_custom_variables.rst
+++ docs/examples/add_custom_variables.rst
@@ -1,6 +1,11 @@
 Add custom variables
 ======================================
 
-This example shows how to add custom variables to the data processing pipeline.
+You can add custom variables using just the name of the variable
+
+.. literalinclude:: ../examples/add_custom_variables_simple.py
+
+You can also add custom variables by initlizing the variable using the Variable class and assigning attributes to it.
+This is useful when you want to add attributes to the NetCDF file for that variable. Like assigning units or adding a comment.
 
 .. literalinclude:: ../examples/add_custom_variables.py
